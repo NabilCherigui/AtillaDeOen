e07d4a47543e1addc84d0122c74ba99238ae8b46
NabilCherigui <mrtopgamer@hotmail.com>
2017-04-10 14:38:22 +0200

    Sorry
    Ik moet ff commiten

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Interact : MonoBehaviour
+{
+    [SerializeField] private int _distance;
+    [SerializeField] private Text _alertInteraction;
+
+    private Vector3 _foreward;
+
+	void Start () {
+	    _foreward = transform.TransformDirection(Vector3.forward);
+	    _alertInteraction = _alertInteraction.GetComponent<Text>();
+	}
+	
+	void Update () {
+
+
+	    if (Physics.Raycast(transform.position, _foreward, _distance))
+	        print("There is something infront of me");
+	}
+}
