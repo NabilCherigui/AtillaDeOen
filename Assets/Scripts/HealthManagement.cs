8acc51eee5f25b8fa7a3de4ae38c25a84eb72721
dice9907 <22070@ma-web.nl>
2017-04-13 13:53:24 +0200

    HealthManager
    Health manager including visual effects

---
+++ Assets/Scripts/HealthManagement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HealthManagement : MonoBehaviour
+{
+    [SerializeField] private Image _damageFilter;
+    [SerializeField] private Image _deathFilter;
+    [SerializeField] private Text _restartText;
+    [SerializeField] private int _maxHealth;
+    private int _currentHealth;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    _restartText.gameObject.SetActive(false);
+	    _currentHealth = _maxHealth;
+	}
+
+    void Update()
+    {
+        if (_currentHealth > 0) return;
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+    private void TakeDamage(int damage)
+    {
+        _currentHealth -= damage;
+        var temp = _damageFilter.color;
+        temp.a = (float)( 1 - 0.34 * _currentHealth);
+        _damageFilter.color = temp;
+        if (_currentHealth <= 0)
+        {
+            Killed();
+        }
+    }
+
+    private void Killed()
+    {
+        var temp = _deathFilter.color;
+        temp.a = 1;
+        _deathFilter.color = temp;
+        _restartText.gameObject.SetActive(true);
+    }
+}
