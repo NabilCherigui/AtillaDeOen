a5eede036fffe221e3302cb796fba365219329af
ninoosewoudt <nino.osewoudt@gmail.com>
2017-04-14 10:29:20 +0200

    Merge branch 'development' into key
    # Conflicts:
    #	.idea/.idea.AtillaDeOen/.idea/workspace.xml
    #	Assets/_Scenes/Main.unity

---
+++ Assets/Scripts/KeyManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class KeyManager : MonoBehaviour {
+
+	[SerializeField] GameObject _key1;
+	[SerializeField] GameObject _key2;
+	[SerializeField] GameObject _empty;
+
+	private int _keyAmount;
+
+	public int KeyAmount {
+		get { 
+			return _keyAmount;
+		}
+	}
+
+	void Update(){
+		if (_key1 == null) {
+			_keyAmount++;
+			_key1 = _empty;
+		}
+	    if (_key2 != null) return;
+	    _keyAmount++;
+	    _key2 = _empty;
+	}
+
+}
