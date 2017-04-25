19217783f1da06420aae54e8400614531def2c1f
NabilCherigui <mrtopgamer@hotmail.com>
2017-04-26 00:13:39 +0200

    Peripheral test
    Working on the peripheral mechanic.

--- Assets/Scripts/Demon.cs
+++ Assets/Scripts/Demon.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
@@ -18,6 +19,42 @@ public class Demon : MonoBehaviour {
 	/** we declareren de statemachine */
 	private StateMachine stateMachine;
 
+    public float viewRadius;
+    [Range(0,360)]
+    public float viewAngle;
+
+    public LayerMask targetMask;
+    public LayerMask obstacleMask;
+
+    void FindVisibleTargets()
+    {
+        Collider[] targetsInViewRadius = Physics.OverlapSphere(transform.position, viewRadius, targetMask);
+
+        for (int i = 0; i < targetsInViewRadius.Length; i++)
+        {
+            Transform target = targetsInViewRadius[i].transform;
+            Vector3 dirToTarget = (target.position - transform.position).normalized;
+            if (Vector3.Angle(transform.forward, dirToTarget) < viewAngle / 2)
+            {
+                float dstToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, dirToTarget, dstToTarget, obstacleMask))
+                {
+                    print("Ik zie je.");
+                }
+            }
+        }
+    }
+
+    public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
+    {
+        if (!angleIsGlobal)
+        {
+            angleInDegrees += transform.eulerAngles.y;
+        }
+        return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees * Mathf.Deg2Rad));
+    }
+
 	// Use this for initialization
 	void Start () {
 		/** we halen een referentie op naar de state machine */
@@ -31,8 +68,9 @@ public class Demon : MonoBehaviour {
 
 	}
 
-    bool Peripheral()
+    void Update() //bool Peripheral()
     {
-        return false;
+        FindVisibleTargets();
+      //  return false;
     }
 }
