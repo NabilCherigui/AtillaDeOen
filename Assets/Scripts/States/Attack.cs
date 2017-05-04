00c992ac4e9a21662886fbd5b171519ca7c593db
NabilCherigui <mrtopgamer@hotmail.com>
2017-05-04 09:31:42 +0200

    Attack
    Attack state of enemy

---
+++ Assets/Scripts/States/Attack.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : State
+{
+
+    public override void Act()
+    {
+        transform.gameObject.tag = "Attack";
+    }
+
+    public override void Reason()
+    {
+        if (!Demon.Peripheral())
+        {
+            GetComponent<StateMachine>().SetState(StateID.Wander);
+        }
+        else
+        {
+            GetComponent<StateMachine>().SetState(StateID.Follow);
+        }
+    }
+}
