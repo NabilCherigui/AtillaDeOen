69dc2fa8cc5fd7e82bdba3080e25f2bb6639579a
NabilCherigui <mrtopgamer@hotmail.com>
2017-05-27 01:52:53 +0200

    KeyUI functional

--- Assets/Scripts/KeyUI.cs
+++ Assets/Scripts/KeyUI.cs
@@ -5,21 +5,43 @@ public class KeyUI : MonoBehaviour {
 
 	[SerializeField] private KeyManager _keyManager;
 	[SerializeField] private Text _text;
-
+	[SerializeField] private int _sizeDecrease;
 	[SerializeField] [Range(0,1)] private float _alphaDecrease;
+
+	private int _normalSize;
+	private int _keyAmount;
+	private float _normalAlpha;
 	private float _alpha;
+
+
 
 	void Start () {
 		_keyManager = _keyManager.GetComponent<KeyManager> ();
-		//_text.gameObject.SetActive (false);
+		_normalAlpha = _text.color.a;
 		_alpha = _text.color.a;
+		_normalSize = _text.fontSize;
+		_keyAmount = _keyManager.KeyAmount;
+		_text.text = "";
 	}
 
 	void Update(){
-		_text.text = _keyManager.KeyAmount.ToString();
-		_alpha -= _alphaDecrease;
-		_text.color = new Color(_text.color.r, _text.color.g, _text.color.b, _alpha);
-		//_text.gameObject.SetActive (true);
+		if (_keyManager.KeyAmount > _keyAmount)
+		{
+			if (_text.fontSize < 0)
+			{
+				_text.fontSize = _normalSize;
+				_keyAmount = _keyManager.KeyAmount;
+				_text.color = new Color(_text.color.r, _text.color.g, _text.color.b, _normalAlpha);
+				_text.text = "";
+			}
+			else
+			{
+				_text.fontSize -= _sizeDecrease;
+				_alpha -= _alphaDecrease;
+				_text.color = new Color(_text.color.r, _text.color.g, _text.color.b, _alpha);
+				_text.text = _keyManager.KeyAmount.ToString();
+			}
+		}
 	}
 
 
